f9628e2b4f7ad6521c4edcdf72bfc88514e12de8
Juhl <rasmus.s.juhl@gmail.com>
2023-10-25 18:43:15 +0200

    Basic mean-variance optimization implemented

--- app.py
+++ app.py
@@ -1,21 +1,53 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import yfinance as yf # for historical stock data
+import numpy as np # for numerical operations
+import pandas as pd # for data manipulation
+from scipy.optimize import minimize # for optimization
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
-    # Instead of rendering an HTML template, you return JSON data.
-    # For demonstration purposes, I'm just returning a simple message. 
-    # In real-world scenarios, you might return relevant data.
     return jsonify({"message": "Welcome to OptiFolio API!"})
 
-# API endpoint to compute optimal portfolio given a list of stock tickers
+def fetch_stock_data(stocks):
+    data = yf.download(stocks, start="2020-01-01", end="2023-01-01")['Adj Close']
+    return data
+
+def compute_portfolio_metrics(data):
+    returns = data.pct_change().dropna()
+    expected_returns = returns.mean()
+    covariance_matrix = returns.cov()
+    correlation_matrix = returns.corr()
+    return expected_returns, covariance_matrix, correlation_matrix
+
+def compute_optimal_portfolio(expected_returns, covariance_matrix, short=False):
+    num_assets = len(expected_returns)
+    args = (expected_returns, covariance_matrix)
+    constraints = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
+    bound = (1 if short else 0, 1)
+    bounds = tuple(bound for asset in range(num_assets))
+    
+    result = minimize(portfolio_volatility, num_assets*[1./num_assets,], args=args, method='SLSQP', bounds=bounds, constraints=constraints)
+    return result.x
+
+def portfolio_volatility(weights, expected_returns, covariance_matrix):
+    return np.sqrt(np.dot(weights.T, np.dot(covariance_matrix, weights)))
+
 @app.route('/api/compute-portfolio', methods=['POST'])
 def compute_portfolio():
     stocks = request.json['stocks']
-    expected_returns, _, correlation_matrix = compute_portfolio_metrics(fetch_stock_data(stocks))
-    optimal_weights = compute_optimal_portfolio(expected_returns, correlation_matrix, False)
+    if len(stocks) == 1:
+        # If only one stock is selected, simply return a weight of 1 for it
+        return jsonify({"weights": [1.0]})
+    
+    data = fetch_stock_data(stocks)
+    expected_returns, covariance_matrix, _ = compute_portfolio_metrics(data)
+    optimal_weights = compute_optimal_portfolio(expected_returns, covariance_matrix)
     return jsonify({"weights": optimal_weights.tolist()})
 
 if __name__ == '__main__':
     app.run(debug=True)
+
--- optifolio/src/components/StockSelect.js
+++ optifolio/src/components/StockSelect.js
@@ -1,13 +1,103 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { PieChart, Pie, Cell } from 'recharts';
 
 function StockSelect() {
+  const [selectedStocks, setSelectedStocks] = useState([]);
+  const [portfolioWeights, setPortfolioWeights] = useState(null);
+  const [hasComputed, setHasComputed] = useState(false);
+  const COLORS = [
+    '#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#FF5733', '#33FF57', 
+    '#8533FF', '#33FFF5', '#FF33F5', '#F5FF33'
+  ];
+
+  const toggleStockSelection = (stock) => {
+    if (selectedStocks.includes(stock)) {
+      setSelectedStocks(selectedStocks.filter(s => s !== stock));
+    } else {
+      setSelectedStocks([...selectedStocks, stock]);
+    }
+  }
+
+  const computeOptimalPortfolio = async () => {
+    if (selectedStocks.length === 0) {
+      setPortfolioWeights(null);
+      return;
+    }
+
+    setHasComputed(true);
+    const response = await fetch('http://localhost:5000/api/compute-portfolio', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ stocks: selectedStocks })
+    });
+
+    const data = await response.json();
+    console.log(data);
+    setPortfolioWeights(data.weights);
+  }
+
+  useEffect(() => {
+    if (hasComputed) {
+      // Reset weights to null every time stock selection changes
+      setPortfolioWeights(null);
+      
+      // Compute the optimal portfolio
+      computeOptimalPortfolio();
+    }
+  }, [selectedStocks]);
 
   return (
     <div className="App">
       <h2>Select Stocks</h2>
       <p>Based on your interest or current portfolio, select stocks below for portfolio optimization:</p>
 
-      <button id="getStarted">CREATE MY PORTFOLIO ALREADY!</button>
+      <div className="stock-container">
+        {['TSLA', 'META', 'AAPL', 'AMZN'].map(stock => (
+          <div 
+            key={stock}
+            className={`stock-box ${selectedStocks.includes(stock) ? 'selected' : ''}`}
+            onClick={() => toggleStockSelection(stock)}
+          >
+            {stock}
+          </div>
+        ))}
+      </div>
+
+      <button id="getStarted" onClick={computeOptimalPortfolio}>CREATE MY PORTFOLIO ALREADY!</button>
+
+      {
+        portfolioWeights && portfolioWeights.length === selectedStocks.length && (
+          <div className="pie-container">
+            <PieChart width={400} height={400}>
+              <Pie
+                data={selectedStocks.map((stock, index) => ({ name: stock, value: portfolioWeights[index] }))}
+                cx={200}
+                cy={200}
+                labelLine={false}
+                outerRadius={80}
+                fill="#8884d8"
+              >
+                {
+                  selectedStocks.map((stock, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+                }
+              </Pie>
+            </PieChart>
+            <div className="legend">
+              {selectedStocks.map((stock, index) => (
+                <div key={stock} className="legend-item">
+                  <span 
+                    className="color-box" 
+                    style={{ backgroundColor: COLORS[index % COLORS.length] }}
+                  ></span>
+                  {stock}: {portfolioWeights[index].toFixed(2)}
+                </div>
+              ))}
+            </div>
+          </div>
+        )
+      }
     </div>
   );
 }
