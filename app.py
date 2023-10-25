46cc4f8162c15a19eb3eb1dcacd0e737fa1c72f1
Juhl <rasmus.s.juhl@gmail.com>
2023-10-25 21:41:49 +0200

    Added Efficient Frontier

--- app.py
+++ app.py
@@ -13,41 +13,79 @@ def index():
     return jsonify({"message": "Welcome to OptiFolio API!"})
 
 def fetch_stock_data(stocks):
-    data = yf.download(stocks, start="2020-01-01", end="2023-01-01")['Adj Close']
+    data = yf.download(stocks, start="2015-01-01", end="2023-01-01")['Adj Close']
     return data
 
 def compute_portfolio_metrics(data):
     returns = data.pct_change().dropna()
-    expected_returns = returns.mean()
+    expected_returns = returns.mean() * 252
     covariance_matrix = returns.cov()
     correlation_matrix = returns.corr()
     return expected_returns, covariance_matrix, correlation_matrix
 
-def compute_optimal_portfolio(expected_returns, covariance_matrix, short=False):
+def compute_optimal_portfolio(expected_returns, covariance_matrix, target_return=None, short=False):
     num_assets = len(expected_returns)
     args = (expected_returns, covariance_matrix)
     constraints = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
+    
+    if target_return:
+        constraints = (
+            {'type': 'eq', 'fun': lambda x: np.sum(x) - 1},
+            {'type': 'eq', 'fun': lambda x: target_return - np.dot(x, expected_returns)}
+        )
+    
     bound = (1 if short else 0, 1)
     bounds = tuple(bound for asset in range(num_assets))
     
-    result = minimize(portfolio_volatility, num_assets*[1./num_assets,], args=args, method='SLSQP', bounds=bounds, constraints=constraints)
+    result = minimize(portfolio_performance, num_assets*[1./num_assets,], args=args, method='SLSQP', bounds=bounds, constraints=constraints)
     return result.x
 
-def portfolio_volatility(weights, expected_returns, covariance_matrix):
-    return np.sqrt(np.dot(weights.T, np.dot(covariance_matrix, weights)))
+def portfolio_performance(weights, expected_returns, covariance_matrix):
+    port_return = np.dot(weights, expected_returns)
+    port_volatility = np.sqrt(np.dot(weights.T, np.dot(covariance_matrix, weights)))
+    sharpe_ratio = port_return / port_volatility
+    return -sharpe_ratio  # negative for maximization
 
 @app.route('/api/compute-portfolio', methods=['POST'])
 def compute_portfolio():
     stocks = request.json['stocks']
     if len(stocks) == 1:
-        # If only one stock is selected, simply return a weight of 1 for it
-        return jsonify({"weights": [1.0]})
-    
+        return jsonify({"weights": [1.0], "frontier": []})
+
     data = fetch_stock_data(stocks)
     expected_returns, covariance_matrix, _ = compute_portfolio_metrics(data)
+    
+    min_return = min(expected_returns)
+    max_return = max(expected_returns)
+    target_returns = np.linspace(min_return, max_return, 12)  # 12 points on the frontier
+
+    frontier = []
+    for target in target_returns:
+        weights = compute_optimal_portfolio(expected_returns, covariance_matrix, target)
+        port_return = np.dot(weights, expected_returns)
+        port_volatility = np.sqrt(np.dot(weights.T, np.dot(covariance_matrix, weights)))
+        frontier.append({"return": port_return, "volatility": port_volatility})
+
+    # Return weights for optimal (tangency) portfolio
     optimal_weights = compute_optimal_portfolio(expected_returns, covariance_matrix)
-    return jsonify({"weights": optimal_weights.tolist()})
+
+    # Tangency Portfolio (highest Sharpe ratio)
+    tangency_portfolio = max(frontier, key=lambda x: x["return"] / x["volatility"])
+
+    # Individual Stock Data
+    individual_stocks = [{"name": stock, "return": expected_returns[stock], "volatility": (covariance_matrix[stock][stock])**0.5} for stock in stocks]
+
+    # Minimum Variance Portfolio
+    min_variance_portfolio = min(frontier, key=lambda x: x["volatility"])
+
+    
+    return jsonify({
+        "weights": optimal_weights.tolist(), 
+        "frontier": frontier, 
+        "tangency": tangency_portfolio,
+        "stocks": individual_stocks,
+        "min_variance": min_variance_portfolio
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
-
--- optifolio/src/components/StockSelect.js
+++ optifolio/src/components/StockSelect.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { PieChart, Pie, Cell } from 'recharts';
+import { PieChart, Pie, Cell, ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 function StockSelect() {
   const [selectedStocks, setSelectedStocks] = useState([]);
   const [portfolioWeights, setPortfolioWeights] = useState(null);
+  const [efficientFrontier, setEfficientFrontier] = useState([]);
+  const [tangencyPortfolio, setTangencyPortfolio] = useState(null);
+  const [individualStocks, setIndividualStocks] = useState([]);
+  const [minVariancePortfolio, setMinVariancePortfolio] = useState(null);
   const [hasComputed, setHasComputed] = useState(false);
   const COLORS = [
     '#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#FF5733', '#33FF57', 
@@ -36,6 +40,11 @@ function StockSelect() {
     const data = await response.json();
     console.log(data);
     setPortfolioWeights(data.weights);
+    setEfficientFrontier(data.frontier);
+    setTangencyPortfolio(data.tangency);
+    setIndividualStocks(data.stocks);
+    setMinVariancePortfolio(data.min_variance);
+
   }
 
   useEffect(() => {
@@ -69,31 +78,68 @@ function StockSelect() {
 
       {
         portfolioWeights && portfolioWeights.length === selectedStocks.length && (
-          <div className="pie-container">
-            <PieChart width={400} height={400}>
-              <Pie
-                data={selectedStocks.map((stock, index) => ({ name: stock, value: portfolioWeights[index] }))}
-                cx={200}
-                cy={200}
-                labelLine={false}
-                outerRadius={80}
-                fill="#8884d8"
+          <div className="chart-container">
+            {/* Container for Efficient Frontier */}
+            <div className="frontier-container">
+            <ScatterChart
+                width={850}
+                height={400}
+                margin={{ top: 50, right: 20, bottom: 20, left: 20 }}
               >
-                {
-                  selectedStocks.map((stock, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
-                }
-              </Pie>
-            </PieChart>
-            <div className="legend">
-              {selectedStocks.map((stock, index) => (
-                <div key={stock} className="legend-item">
-                  <span 
-                    className="color-box" 
-                    style={{ backgroundColor: COLORS[index % COLORS.length] }}
-                  ></span>
-                  {stock}: {portfolioWeights[index].toFixed(2)}
-                </div>
-              ))}
+                <CartesianGrid />
+                <XAxis 
+                  type="number" 
+                  dataKey={entry => entry.volatility * Math.sqrt(252)}
+                  name="Volatility"
+                  unit="%"
+                  tickFormatter={(value) => value.toFixed(2)}
+                  label={{ value: 'Volatility', position: 'bottom' }} 
+                />
+                <YAxis 
+                  type="number" 
+                  dataKey={entry => entry.return * 100}
+                  name="Expected Return" 
+                  unit="%"
+                  tickFormatter={(value) => value.toFixed(1)}
+                  label={{ value: 'Expected Return', position: 'outsideLeft', angle: -90, dx: -40, dy: -0 }} 
+                />
+
+                <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+                <Legend align="right" verticalAlign="middle" layout="vertical" wrapperStyle={{ paddingLeft: '40px' }} />
+                <Scatter name="Efficient Frontier" data={efficientFrontier} fill="#36A2EB" line shape="circle" />
+                <Scatter name="Tangency Portfolio" data={[tangencyPortfolio]} fill="#FFCE56" shape="diamond" />
+                <Scatter name="Individual Stocks" data={individualStocks} fill="#4BC0C0" shape="circle" />
+                <Scatter name="Min Variance Portfolio" data={[minVariancePortfolio]} fill="#FF5733" shape="triangle" />
+              </ScatterChart>
+            </div>
+
+            {/* Container for Pie Chart and Legend */}
+            <div className="portfolio-container">
+              <PieChart width={400} height={400}>
+                <Pie
+                  data={selectedStocks.map((stock, index) => ({ name: stock, value: portfolioWeights[index] }))}
+                  cx={200}
+                  cy={200}
+                  labelLine={false}
+                  outerRadius={150}
+                  fill="#8884d8"
+                >
+                  {
+                    selectedStocks.map((stock, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+                  }
+                </Pie>
+              </PieChart>
+              <div className="legend">
+                {selectedStocks.map((stock, index) => (
+                  <div key={stock} className="legend-item">
+                    <span 
+                      className="color-box" 
+                      style={{ backgroundColor: COLORS[index % COLORS.length] }}
+                    ></span>
+                    {stock}: {portfolioWeights[index].toFixed(2)}
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         )
